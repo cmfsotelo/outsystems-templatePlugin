d137d0300fa664fabd8b97e783ff1c515c4d1e26
Carlos Sotelo <carlos.sotelo@outsystems.com>
2023-04-26 07:17:36 +0100

    feat: adding trapeze

---
+++ scripts/trapeze.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+const logger = require('cordova-common').CordovaLogger.get();
+const { execSync } = require("child_process");
+const ExtendedConfigParser = require('./utils/extendedConfigParser');
+
+
+const PREFERENCE_NAME_SUFFIX = 'trapeze-conf'
+
+module.exports = function (context) {
+    debugger
+    // Get the Cordova project directory
+    const projectRoot = context.opts.projectRoot;
+    // Get the platform (android or ios)
+    const platform = context.opts.cordova.platforms[0];
+
+    let yamlContents = null;
+
+    // Get preferences that describe usage descriptions
+    const parser = ExtendedConfigParser.createInstance(context);
+    const preferenceValue = parser
+        .getPreference(PREFERENCE_NAME_SUFFIX, platform);
+
+
+    // Look for a trapeze-platform.yaml file in the root of the platform directory
+    const yamlPath = path.join(projectRoot, 'platforms', platform, 'trapeze-conf.yaml');
+
+    if (preferenceValue) {
+        try {
+            // Decode the base64-encoded value
+            const decodedValue = Buffer.from(preferenceValue, 'base64').toString('utf-8');
+            yamlContents = decodedValue.trim();
+            yamlContents = fs.writeFileSync(yamlPath, 'utf-8').trim();
+        } catch (e) {
+            logger.error(`Invalid base64-encoded value for preference ${preferenceName}`);
+            logger.error(e);
+            return;
+        }
+    }
+
+    if (!fs.existsSync(yamlPath)) {
+        logger.error(`Could not find Trapeze YAML file in path ${yamlPath}`);
+        return;
+    }
+
+    let command = `npx trapeze run ${yamlPath} -y`;
+    const platformPath = path.join(projectRoot, 'platforms', platform);
+    if (platform == "android") {
+        command = command.concat(" --android-project ", platformPath);
+    } else if (platform == "ios") {
+        command =command.concat(" --ios-project ", platformPath);
+    }
+
+    let result = runCommand(command);
+
+    result ?
+        console.log("Result: ", result.toString()) :
+        console.error(" > Failed to perform the operation: ", result);
+
+};
+
+/**
+ * Tries to install a tool with the command supplied
+ * @param {string} cmd command that will install the tool
+ * @returns {Boolean} indicating the success of the attack
+ */
+function runCommand(cmd) {
+    console.log(`Running the command ${cmd}...`);
+    return execSync(cmd);
+}
